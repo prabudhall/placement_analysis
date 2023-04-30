df06c195eb75555d24fa487e340345577317fb85
Prabudh Kumar <prabudh2002@gmail.com>
2023-04-30 21:03:57 +0530

    added model for dbms

---
+++ try/filter_csv.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9561802e6f741d76d9d7ab364a7519aacbbc2f770fd0cfbf6749d8915a7a9f8
+size 827
---
+++ try/txt_to_csv.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e242e80d79e93e41e76cdf3c1bd4139a2ecb3ca3714713e0c43abde3817034b1
+size 263
